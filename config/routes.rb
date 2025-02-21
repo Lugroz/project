8701e5a715e11d22f1bc6609732edafdb59ebcc1
Lugroz <lgruiz7.lr@gmail.com>
2025-02-20 19:24:24 -0600

    Fix GitHub Actions workflow and dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
- post '/users', to: 'users#create'
-  get '/posts', to: 'posts#index'
-  post '/posts', to: 'posts#create'
-  put '/posts/:id', to: 'posts#update'
-  delete '/posts/:id', to: 'posts#destroy'
+ post "/users", to: "users#create"
+  get "/posts", to: "posts#index"
+  post "/posts", to: "posts#create"
+  put "/posts/:id", to: "posts#update"
+  delete "/posts/:id", to: "posts#destroy"
  end
